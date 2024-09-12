3c1dbe2279918c6b54beca1a8c24cb75ed7567d2
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-09-12 09:06:47 -0500

    chore: initial commit...upgraded to dotnet 8 and fixed vue cdn to use compatible version

---
+++ README.md
@@ -0,0 +1,3 @@
+# Data Access Patterns
+
+This is code I wrote while studying data access patterns in this [Pluralsight course](https://app.pluralsight.com/library/courses/c-sharp-design-patterns-data-access-patterns/table-of-contents).
---
+++ src/MyShop.Web/Startup.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using MyShop.Domain.Models;
+using MyShop.Infrastructure;
+
+namespace MyShop.Web
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.Configure<CookiePolicyOptions>(options =>
+      {
+        options.CheckConsentNeeded = context => true;
+        options.MinimumSameSitePolicy = SameSiteMode.None;
+      });
+
+      services.AddRazorPages();
+
+      CreateInitialDatabase();
+
+      services.AddTransient<ShoppingContext>();
+
+    }
+
+    public void CreateInitialDatabase()
+    {
+      using (var context = new ShoppingContext())
+      {
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+
+        var camera = new Product { Name = "Canon EOS 70D", Price = 599m };
+        var microphone = new Product { Name = "Shure SM7B", Price = 245m };
+        var light = new Product { Name = "Key Light", Price = 59.99m };
+        var phone = new Product { Name = "Android Phone", Price = 259.59m };
+        var speakers = new Product { Name = "5.1 Speaker System", Price = 799.99m };
+
+        context.Products.Add(camera);
+        context.Products.Add(microphone);
+        context.Products.Add(light);
+        context.Products.Add(phone);
+        context.Products.Add(speakers);
+
+        context.SaveChanges();
+      }
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Order/Error");
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+      app.UseCookiePolicy();
+
+      app.UseRouting();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllerRoute(
+          name: "default",
+          pattern: "{controller=Order}/{action=Index}/{id?}"
+        );
+        endpoints.MapRazorPages();
+      });
+    }
+  }
+}

d2f81f57e03369c911439e537d264e1c3add2cf5
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-09-13 09:45:04 -0500

    chore: register repositories and stop deleting db on restarts.

--- src/MyShop.Web/Startup.cs
+++ src/MyShop.Web/Startup.cs
@@ -1,16 +1,12 @@
 ﻿using MyShop.Domain.Models;
 using MyShop.Infrastructure;
+using MyShop.Infrastructure.Repositories;
 
 namespace MyShop.Web
 {
-  public class Startup
+  public class Startup(IConfiguration configuration)
   {
-    public Startup(IConfiguration configuration)
-    {
-      Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
+    public IConfiguration Configuration { get; } = configuration;
 
     public void ConfigureServices(IServiceCollection services)
     {
@@ -24,31 +20,30 @@ namespace MyShop.Web
 
       CreateInitialDatabase();
 
-      services.AddTransient<ShoppingContext>();
-
+      services.AddDbContext<ShoppingContext>();
+      services.AddScoped<IRepository<Customer>, CustomerRepository>();
+      services.AddScoped<IRepository<Order>, OrderRepository>();
+      services.AddScoped<IRepository<Product>, ProductRepository>();
     }
 
     public void CreateInitialDatabase()
     {
-      using (var context = new ShoppingContext())
-      {
-        context.Database.EnsureDeleted();
-        context.Database.EnsureCreated();
+      using var context = new ShoppingContext();
+      context.Database.EnsureCreated();
 
-        var camera = new Product { Name = "Canon EOS 70D", Price = 599m };
-        var microphone = new Product { Name = "Shure SM7B", Price = 245m };
-        var light = new Product { Name = "Key Light", Price = 59.99m };
-        var phone = new Product { Name = "Android Phone", Price = 259.59m };
-        var speakers = new Product { Name = "5.1 Speaker System", Price = 799.99m };
+      var camera = new Product { Name = "Canon EOS 70D", Price = 599m };
+      var microphone = new Product { Name = "Shure SM7B", Price = 245m };
+      var light = new Product { Name = "Key Light", Price = 59.99m };
+      var phone = new Product { Name = "Android Phone", Price = 259.59m };
+      var speakers = new Product { Name = "5.1 Speaker System", Price = 799.99m };
 
-        context.Products.Add(camera);
-        context.Products.Add(microphone);
-        context.Products.Add(light);
-        context.Products.Add(phone);
-        context.Products.Add(speakers);
+      context.Products.Add(camera);
+      context.Products.Add(microphone);
+      context.Products.Add(light);
+      context.Products.Add(phone);
+      context.Products.Add(speakers);
 
-        context.SaveChanges();
-      }
+      context.SaveChanges();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -66,7 +61,6 @@ namespace MyShop.Web
       app.UseHttpsRedirection();
       app.UseStaticFiles();
       app.UseCookiePolicy();
-
       app.UseRouting();
       app.UseEndpoints(endpoints =>
       {
